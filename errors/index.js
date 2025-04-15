89f3f8f0c6bc6fd485292d36e4bb70479b532eb7
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2025-04-15 15:40:58 +0300

    backend spaces

---
+++ errors/index.js
@@ -0,0 +1,6 @@
+import BadRequestError from "./bad-request.js"
+import NotFoundError from "./not-found.js"
+import UnauthenticatedError from "./unauthenticated.js"
+import UnauthorizedError from "./unauthorized.js"
+
+export { BadRequestError, NotFoundError, UnauthenticatedError, UnauthorizedError }
